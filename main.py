811d3cc78169529236022ad438c5667ac9b34393
Sobelema <s.atemie@se16.qmul.ac.uk>
2025-03-15 02:00:30 +0000

    complete tested rss feed

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import feedparser
 
-url = input("Enter the RSS feed URL: ")
-newsFeed = feedparser.parse(url)
-entry = newsFeed.entries[1]
+def get_url():
+    url = input("Enter the RSS feed URL: ")
+    return url
 
-print('Post Title:', entry.title)
-print('Post Description:', entry.summary)
-print('Post Link:', entry.link)
+def get_rss_feed(url):
+    newsFeed = feedparser.parse(url)
+    entry = newsFeed.entries[1]
+    return entry
+    # print(entry.keys())
+
+def print_rss_feed(entry):
+    print('Post Title:', entry.title)
+    print('Post Description:', entry.summary)
+    print('Post Link:', entry.link)
+
+def main():
+    url = get_url()
+    entry = get_rss_feed(url)
+    print_rss_feed(entry)
+
+if __name__ == '__main__':
+    main()
